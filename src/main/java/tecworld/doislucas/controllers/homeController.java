b5a7a569191880d9c93e2daeda92e471492653a1
Pedro Lucas <pedrolucas499@gmail.com>
2018-08-18 01:25:35 -0300

    Index Page

---
+++ src/main/java/tecworld/doislucas/controllers/homeController.java
@@ -0,0 +1,14 @@
+package tecworld.doislucas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "indexx";
+    }
+}
