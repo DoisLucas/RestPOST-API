8b326a72cd8f4722f74cdfdd6fa77f4a72e249e7
DoisLucas <pedrolucas499@gmail.com>
2019-03-17 22:51:50 -0300

    Ajustes HttpStatus

--- src/main/java/tecworld/doislucas/controllers/emailController.java
+++ src/main/java/tecworld/doislucas/controllers/emailController.java
@@ -23,7 +23,7 @@ public class emailController {
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body( emailService.addEmail(email) );
         } catch (DataIntegrityViolationException e) {
-            return ResponseEntity.status(HttpStatus.FOUND).body("Error: Duplicate key value violates unique constraint");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Duplicate key value violates unique constraint");
         }
     }
 
